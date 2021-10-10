7c34e72a5c597c133b2d2086161c7919354eaa01
sngtmth <sngtmth@gmail.com>
2021-10-11 00:49:39 +0530

    Added:Choose player and computer Letter X or O

--- src/com/bridge/tictactoe/TicTacToeGame.java
+++ src/com/bridge/tictactoe/TicTacToeGame.java
@@ -1,17 +1,37 @@
 
 package com.bridge.tictactoe;
 
+import java.util.Scanner;
+
 /**
  * TicTacToeGame Program
  * 
  * @author Sangeeta Math
  * 
- *
  */
 public class TicTacToeGame {
 
+	private static char player;
+	private static char computer;
+
 	public static void main(String[] args) {
 		char[] board = createBoard();
+		player = ChooseLetter();
+		if (player == 'X') {
+			computer = '0';
+		} else {
+			computer = 'X';
+		}
+		System.out.println("Computer Letter is=>" + computer + " " + "Player letter is=>" + player);
+
+	}
+
+	private static char ChooseLetter() {
+		System.out.println("Choose letters for Player and computer");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter For Player(X/0)");
+		player = sc.next().charAt(0);
+		return player;
 	}
 
 	/*
